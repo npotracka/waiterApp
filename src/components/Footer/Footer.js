00d717155468ad2227b82692532ccf93111775d2
Natalia Potracka <n.potracka@gmail.com>
2023-08-31 13:58:11 +0200

    add header, footer, navbar and changed home

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className='text-muted text-center my-4'>
+      Copyright © PizzeriaApp 2023
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,30 @@
+import { Button, ListGroup, Stack } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { getAllTables } from '../../redux/tablesRedux';
+
 const Home = () => {
+  const tables = useSelector(getAllTables);
   return (
-    <>
-      Waiter App
-    </>
-  )
-}
+    <div>
+      <h1 className='my-4'>All TABLES</h1>
+      <ListGroup variant='flush'>
+        {tables.map((table) => (
+          <ListGroup.Item key={table.id} {...table} className='px-0'>
+            <Stack direction='horizontal' gap={4}>
+              <h2>Table {table.id}</h2>
+              <p className='mb-1'>
+                <strong>Status: </strong>
+                {table.status}
+              </p>
+              <Button variant='primary' className='ms-auto'>
+                Show more
+              </Button>
+            </Stack>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </div>
+  );
+};
 
 export default Home;
